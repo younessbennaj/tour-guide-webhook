eee130238ca1a08cae1ea81f954d643320594d9f
Youness BENNAJ <youness.bennaj@gmail.com>
2019-01-16 18:51:07 +0100

    connect the activities API

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = 8000;
+const API_URL = "https://f6a23ece.ngrok.io/api/activities";
 
 app.get("/", (req, res) => {
-  res.send("Hello World !");
+  axios
+    .get(API_URL, {
+      params: {
+        tags: ["museum", "culture"]
+      }
+    })
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(port, () => console.log(`Server en écoute sur le port ${port}`));
